d1d08be83683a71514f5307173c995d9254a22ee
yuuuuunaaaaa <9354yuna@gmail.com>
2026-04-10 00:03:16 +0900

    흠

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["mysql2"],
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
